2488db63e84757dd0a47c9512e24f842749cf732
Joseph Rubino <joeyrubi22@gmail.com>
2022-06-02 16:53:29 -0500

    fixed cors file

--- routes/cors.js
+++ routes/cors.js
@@ -1,13 +1,13 @@
 const cors = require('cors');
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3443'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
 const corsOptionsDelegate = (req, callback) => {
     let corsOptions;
     console.log(req.header('Origin'));
     if(whitelist.indexOf(req.header('Origin')) !== -1) {
         corsOptions = { origin: true };
     } else {
-        corsOptions = {origin: false };
+        corsOptions = { origin: false };
     }
     callback(null, corsOptions);
 };
